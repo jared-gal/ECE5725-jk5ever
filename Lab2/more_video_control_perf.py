c703bf568b980c1bb635f78b582388f50e960cb2
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-10-21 13:52:53 -0400

    Update more_video_control_perf.py

--- Lab2/more_video_control_perf.py
+++ Lab2/more_video_control_perf.py
@@ -4,7 +4,10 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
+#getting the start time
 st = time.time()
+
+#setting up the buttons
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -13,7 +16,7 @@ GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN)
 GPIO.setup(19, GPIO.IN)
 
-
+#eval function for button presses
 def case_eval(button):
     switcher = {
         15:"seek -30 0",
@@ -28,6 +31,7 @@ def case_eval(button):
 
 if __name__ == "__main__":
 
+	#base state of buttons
 	button1 = 1
 	button2 = 1
 	button3 = 1
@@ -37,15 +41,18 @@ if __name__ == "__main__":
 	
         
         command = " cat"
-
+	
+	#run polling algo for 10 seconds for comparison with perf
 	while((time.time()-st) < 10):
+		
+	    #same button logic as before
 	    button1 = GPIO.input(17)
 	    button2 = GPIO.input(22)
 	    button3 = GPIO.input(23)
 	    button4 = GPIO.input(27)
             button5 = GPIO.input(26)
             button6 = GPIO.input(19)
-
+	
 	    button = button1 + button2*2 + button3*3 + button4*4 + (1-button5)*5 + (1-button6)*6
 	    command = case_eval(button)
             
