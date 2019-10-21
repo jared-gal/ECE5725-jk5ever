9a6739548cd32bba294ebb02f6e2b4154b89b9cd
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-10-21 13:49:29 -0400

    Update more_video_control_cb_perf.py

--- Lab2/more_video_control_cb_perf.py
+++ Lab2/more_video_control_cb_perf.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
+#setting up the buttons
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -12,7 +13,7 @@ GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN)
 GPIO.setup(19, GPIO.IN)
 
-
+#setting up callbacks
 def gpio17(channel):
     cmd = "echo seek -10 0  > /tmp/mplayer-fifo"
     print subprocess.check_output(cmd, shell=True)
@@ -41,6 +42,7 @@ def gpio19(channel):
 
 if __name__ == "__main__":
 
+	#creating callback ties
     GPIO.add_event_detect(17, GPIO.FALLING, callback = gpio17, bouncetime =300)	
     GPIO.add_event_detect(22, GPIO.FALLING, callback = gpio22, bouncetime =300)
     GPIO.add_event_detect(23, GPIO.FALLING, callback = gpio23, bouncetime =300)
@@ -48,6 +50,7 @@ if __name__ == "__main__":
     GPIO.add_event_detect(26, GPIO.RISING, callback = gpio26, bouncetime =300)
     GPIO.add_event_detect(19, GPIO.RISING, callback = gpio19, bouncetime =300)
 
+	#sleep for perf evaluation time
     time.sleep(10)
 
 
