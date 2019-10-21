e50a5be63c7c838e273970d828bd3dc082f664ce
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-10-21 13:59:14 -0400

    Update six_buttons.py

--- Lab2/six_buttons.py
+++ Lab2/six_buttons.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 
+#button setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -11,6 +12,7 @@ GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN)
 GPIO.setup(19, GPIO.IN)
 
+#initial state of all buttons
 button1 = 1
 button2 = 1
 button3 = 1
@@ -19,6 +21,7 @@ button5 = 0
 button6 = 0
 
 while(1):
+    #polling all buttons
     button1 = GPIO.input(17)
     button2 = GPIO.input(22)
     button3 = GPIO.input(23)
@@ -26,6 +29,7 @@ while(1):
     button5 = GPIO.input(26)
     button6 = GPIO.input(19)
 
+    #display what button is pressed
     if(button1 == 0):
         print "BUTTON 17 IS PRESSED"  
     if(button2 == 0):
@@ -38,7 +42,7 @@ while(1):
         print "BUTTON 26 IS PRESSED"
     if(button6 == 1):
         print "BUTTON 19 IS PRESSED"
-
+        #quit button
         break
         
     time.sleep(.1)
