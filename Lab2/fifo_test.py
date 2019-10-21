908fd8906be4624c05cf8eb65c5815682cea3cc5
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-10-21 13:44:53 -0400

    Update fifo_test.py

--- Lab2/fifo_test.py
+++ Lab2/fifo_test.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-
+#function to decode the button pressed to a command
 def case_eval(command):
     switcher = {
         "pause":"pause",
@@ -17,11 +17,13 @@ if __name__ == "__main__":
 
     usr_in = "cat"
 
+    #reading user input 
     while( usr_in != "quit"):
 
         usr_in = raw_input("Enter a Command: \n")
         command = case_eval(usr_in)  
-
+        
+        #if the user command is valid send it to the fifo
         if(command != "Invalid Command"):
             cmd = "echo "+command+" > /tmp/mplayer-fifo"
             print subprocess.check_output(cmd, shell=True)
