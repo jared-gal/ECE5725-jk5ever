637b36c83379b13037ac36b4813c86885aac14e7
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-10-21 13:48:41 -0400

    Update more_video_control_cb.py

--- Lab2/more_video_control_cb.py
+++ Lab2/more_video_control_cb.py
@@ -4,6 +4,8 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 import sys
+
+#setting up all gpio pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -12,7 +14,7 @@ GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN)
 GPIO.setup(19, GPIO.IN)
 
-
+#callbacks for each of the buttons and their corresponding actions
 def gpio17(channel):
     cmd = "echo seek -10 0  > /tmp/mplayer-fifo"
     print subprocess.check_output(cmd, shell=True)
@@ -42,6 +44,7 @@ def gpio19(channel):
 
 if __name__ == "__main__":
 
+	#setting up callbacks for the buttons
     GPIO.add_event_detect(17, GPIO.FALLING, callback = gpio17, bouncetime =300)	
     GPIO.add_event_detect(22, GPIO.FALLING, callback = gpio22, bouncetime =300)
     GPIO.add_event_detect(23, GPIO.FALLING, callback = gpio23, bouncetime =300)
