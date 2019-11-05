db9550a88002c1baa9730ddf9ab88192826d0c8e
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-11-05 12:59:16 -0500

    Update blink.py

--- Lab3/blink.py
+++ Lab3/blink.py
@@ -6,12 +6,13 @@ import time
 os.putenv('SDL_VIDEODRIVER','fbcon')
 os.putenv('SDL_FBDEV','/dev/fb0')
 
+#setting up gpio pin for pwm
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(4, GPIO.OUT)
 led_pin = GPIO.PWM(4, 1)
 led_pin.start(50)
 
+#taking user input to change the frequency of the PWM signal
 while(1):
     usr_in = 1
     usr_in = raw_input("Enter a Frequency: \n")
