c5aa1e8b861a425276e4e5b85890e25271d4a45c
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-11-05 13:18:30 -0500

    Update run_test.py

--- Lab3/run_test.py
+++ Lab3/run_test.py
@@ -38,13 +38,12 @@ BLACK = 0,0,0
 RED = 255,0,0
 GREEN = 0,255,0
 
-#setting up buttons and blacking out the screen
+#setting up fonts and buttons and blacking out the screen
 my_fontB = pygame.font.Font(None, 50)
 my_fontS = pygame.font.Font(None, 25)
 my_buttons_cont = {'CONT':(160,120),'quit':(280, 200)}
 my_buttons_stop = {'STOP':(160,120),'quit':(280, 200)}
 my_buttons_start = {'START':(160,120), 'quit':(280,200)}
-
 screen.fill(BLACK)
 
 
@@ -91,6 +90,7 @@ if __name__ == "__main__":
     #to record what state we were in before emergency stopping and duration through it
     last_state = 0 
     start_time = 0
+    stop_time
 
     #whether or not quit button was hit
     Quit = False
@@ -100,30 +100,7 @@ if __name__ == "__main__":
     
     while(not Quit):
         time.sleep(.01)
-        #update rectangles
-        
-        '''
-            left_time = round(time.time()-begin_time)
-            left_3_R = left_2_R
-            left_2_R = left_1_R
-            left_2   = left_2_R.get_rect(center=(40,133))
-            left_3   = left_3_R.get_rect(center=(40,166))
-            left_1_R = my_fontS.render(left_dir+", "+str(left_time), True, WHITE)
-            left_1 = left_1_R.get_rect(center=(40,100))
-            
-            
-            
-            right_time = round(time.time()-begin_time)
-            right_3_R = right_2_R
-            right_2_R = right_1_R
-            right_2   = right_2_R.get_rect(center=(280,133))
-            right_3   = right_3_R.get_rect(center=(280,166))
-            right_1_R = my_fontS.render(right_dir+", "+str(right_time), True, WHITE)
-            right_1 = right_1_R.get_rect(center=(280,100))
-            
-        
-        '''
-        
+
         screen.fill(BLACK)
         screen.blit(left_1_R, left_1)
         screen.blit(left_2_R, left_2)
@@ -133,7 +110,7 @@ if __name__ == "__main__":
         screen.blit(right_3_R, right_3)
         
     
-        
+        #reading TFT button presses
         for event in pygame.event.get(): 
             if(event.type is MOUSEBUTTONDOWN): 
                 pos = pygame.mouse.get_pos()
@@ -141,9 +118,11 @@ if __name__ == "__main__":
                 pos = pygame.mouse.get_pos()
                 x,y = pos
 
+                #quit button is hit
                 if y>175: 
                     if x>255: 
                         Quit = True
+                #center button is hit
                 elif y<145 and y>95 and x>135 and x<185:
                     #start state
                     if state ==0:
@@ -155,17 +134,19 @@ if __name__ == "__main__":
                     elif state ==1:
                         my_buttons = my_buttons_stop
                         state = last_state
-                        start_time = time.time()
-                        new_state = True
+                        start_time = time.time() - stop_time
+                        
                     #hit while going -> go to stop state and show continue button
                     else:
+                        stop_time = time.time() - start_time
                         my_buttons = my_buttons_cont
                         last_state = state
                         state = 1
                         new_state = True
 
+        #start of the finite state machine to handle the transitions, explanation of states found at state variable above                
         if state == 0:
-            print("STart")
+            print("Start")
             my_buttons = my_buttons_start
             #update freq and duty to be 0 for both
             servo_pinL.start(0)
@@ -201,7 +182,8 @@ if __name__ == "__main__":
             
         #GO STRAIGHT
         elif state ==2:
-            print("STraight")
+            print("Straight")
+            #update the histories for both wheels
             if new_state:
                 new_state = False
                 start_time = time.time()
@@ -214,8 +196,6 @@ if __name__ == "__main__":
                 left_1_R = my_fontS.render("CCW"+", "+str(left_time), True, WHITE)
                 left_1 = left_1_R.get_rect(center=(40,100))
                 
-                
-                
                 right_time = round(time.time()-begin_time)
                 right_3_R = right_2_R
                 right_2_R = right_1_R
@@ -223,7 +203,7 @@ if __name__ == "__main__":
                 right_3   = right_3_R.get_rect(center=(280,166))
                 right_1_R = my_fontS.render("CW"+", "+str(right_time), True, WHITE)
                 right_1 = right_1_R.get_rect(center=(280,100))
-                
+            #transition states if time is exceed
             if (time.time() - start_time > STRAIGHT_TIME):
                 state = 3
                 new_state = True
@@ -233,10 +213,12 @@ if __name__ == "__main__":
             servo_pinR.ChangeDutyCycle(6.54)
             servo_pinL.ChangeFrequency(46.296)
             servo_pinL.ChangeDutyCycle(7.407)
+            #draw the center circle
             pygame.draw.circle(screen, RED, [160,120], 60)
             
         #GO BACK
         elif state ==3:
+            #update histories for both wheels and start a new start_time
             if new_state:
                 new_state = False
                 start_time = time.time()
@@ -258,7 +240,7 @@ if __name__ == "__main__":
                 right_3   = right_3_R.get_rect(center=(280,166))
                 right_1_R = my_fontS.render("CCW"+", "+str(right_time), True, WHITE)
                 right_1 = right_1_R.get_rect(center=(280,100))
-                
+            #if time limit hit transition to next state
             if (time.time() - start_time > STRAIGHT_TIME):
                 state = 4
                 new_state = True
@@ -268,10 +250,12 @@ if __name__ == "__main__":
             servo_pinL.ChangeDutyCycle(6.54)
             servo_pinR.ChangeFrequency(46.296)
             servo_pinR.ChangeDutyCycle(7.407)
+            #draw appropriate circle
             pygame.draw.circle(screen, RED, [160,120], 60)
             
         #TURN LEFT
         elif state ==4:
+            #update histories for both wheels and start a new start_time
             if new_state:
                 new_state = False
                 start_time = time.time()
@@ -293,7 +277,7 @@ if __name__ == "__main__":
                 right_3   = right_3_R.get_rect(center=(280,166))
                 right_1_R = my_fontS.render("CW"+", "+str(right_time), True, WHITE)
                 right_1 = right_1_R.get_rect(center=(280,100))
-                
+            #if time limit hit transition state    
             if (time.time() - start_time > TURN_TIME):
                 state = 5
                 new_state = True
@@ -307,6 +291,7 @@ if __name__ == "__main__":
             
         #TURN RIGHT
         elif state ==5:
+            #update histories for both wheels and start a new start_time
             if new_state:
                 new_state = False
                 start_time = time.time()
@@ -328,7 +313,7 @@ if __name__ == "__main__":
                 right_3   = right_3_R.get_rect(center=(280,166))
                 right_1_R = my_fontS.render("STOP"+", "+str(right_time), True, WHITE)
                 right_1 = right_1_R.get_rect(center=(280,100))
-                
+            #if time limit hit transition the sate   
             if (time.time() - start_time > TURN_TIME):
                 state = 2
                 new_state = True
@@ -339,12 +324,12 @@ if __name__ == "__main__":
             servo_pinL.ChangeFrequency(46.296)
             servo_pinL.ChangeDutyCycle(7.407)
             pygame.draw.circle(screen, RED, [160,120], 60)
+        #base case to just stop
         else:
-            servo_pinL.ChangeFrequency(46.51)
-            servo_pinL.ChangeDutyCycle(6.977)
-            servo_pinR.ChangeFrequency(46.51)
-            servo_pinR.ChangeDutyCycle(6.977)
+            servo_pinL.ChangeDutyCycle(0)
+            servo_pinR.ChangeDutyCycle(0)
             
+         #displaying appropriate buttons to the screen   
         for my_text, text_pos in my_buttons.items():
             text_surface = my_fontB.render(my_text, True, WHITE)
             rect = text_surface.get_rect(center=text_pos)
