ebe3a3dbeb823684fe2fcdfd0bdf3c3b60171e66
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-11-05 13:00:12 -0500

    Update pwm_calibrate.py

--- Lab3/pwm_calibrate.py
+++ Lab3/pwm_calibrate.py
@@ -6,13 +6,13 @@ import time
 os.putenv('SDL_VIDEODRIVER','fbcon')
 os.putenv('SDL_FBDEV','/dev/fb0')
 
+#setting up GPIO pin to run the calibration signal with f = 46.51 and duty cycle = 6.977%
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(5, GPIO.OUT)
 servo_pin = GPIO.PWM(5, 46.51)
 servo_pin.start(6.977)
 
-
+#reading user input as to when to end the program
 usr_in = "cat"
 while (usr_in != "done"):
     usr_in = raw_input("type done when done")
